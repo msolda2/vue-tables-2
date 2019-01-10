3881be7c0ee2b74dc386daf3d64d8c4a9a1baf30
msolda2 <42726276+msolda2@users.noreply.github.com>
2019-01-09 20:25:04 -0500

    Update defaults.js

--- compiled/config/defaults.js
+++ compiled/config/defaults.js
@@ -32,7 +32,7 @@ module.exports = function () {
     columnsDisplay: {},
     columnsDropdown: false,
     texts: {
-      count: "Showing {from} to {to} of {count} records|{count} records|One record",
+      count: "Showing {from} to {to} of {count} Records|{count} Records|One Record",
       first: 'First',
       last: 'Last',
       filter: "",
